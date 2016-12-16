71608236ee3897f95c94b6d346b26bd297add6b5
zhgzzy <ok_peter@126.com>
2016-12-16 18:06:19 +0800

    add Boolean.js

---
+++ Boolean.js
@@ -0,0 +1,26 @@
+//boolean
+console.log(true);//true
+console.log(false);//flase
+
+console.log(true && true);//true
+console.log(true && false);//false
+
+console.log(true || true);//true
+console.log(true || false);//true
+
+console.log(false || false);//false
+
+
+console.log(!true);//false
+console.log(!false);//true
+
+console.log(2>1);//true
+console.log(2<1);//false
+
+console.log(false == 0);//true
+console.log(false === 0);//false
+
+console.log(NaN === NaN);//false
+console.log(NaN == NaN);//false
+
+
